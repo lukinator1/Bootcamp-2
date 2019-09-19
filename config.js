ecda74ec3bcd0969444e581caacfa936a7eb8f20
lukinator1 <32145995+lukinator1@users.noreply.github.com>
2019-09-19 18:42:50 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    db: {
-      uri: 'mongodb+srv://guest:readycheck@cluster0-qzi2i.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
-    }
-  };
